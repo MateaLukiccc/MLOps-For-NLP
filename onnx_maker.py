3957ef01dc695abf5536a5e8e44fb9536cfcae04
MateaLukiccc <ml20200410@student.fon.bg.ac.rs>
2024-12-24 23:58:57 +0100

    added distilbert files

--- onnx_maker.py
+++ onnx_maker.py
@@ -2,8 +2,9 @@ from optimum.onnxruntime import ORTModelForSequenceClassification
 from transformers import AutoTokenizer
 import onnx
 
-model_checkpoint = "electra_trainer"
-save_directory = "electra_onnx/"
+# example with distilbert model
+model_checkpoint = "distilbert_trainer"
+save_directory = "distibert_onnx/"
 
 # Load a model from transformers and export it to ONNX
 ort_model = ORTModelForSequenceClassification.from_pretrained(model_checkpoint, export=True)
@@ -14,5 +15,5 @@ ort_model.save_pretrained(save_directory)
 tokenizer.save_pretrained(save_directory)
 
 # Check the onnx model
-onnx_model = onnx.load("electra_onnx/model.onnx")
+onnx_model = onnx.load("distibert_onnx\\model.onnx")
 onnx.checker.check_model(onnx_model)
